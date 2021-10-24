db450ec962b80a98c729676d608164b3a5ed202d
Naji Dia <najimdia@gmail.com>
2021-10-23 19:14:28 -0700

    dates and containers

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-// // var today = dayjs.format();
-// $("currentDay").text(today.format("MMM DD, YYYY"))
+var today = dayjs();
+$("currentDay").text(today.format('MMM DD, YYYY h:mm A'))
 
+var currentHour = dayjs().hour();
 
-for (var i = 9; i < 18; i++) {
-    $(".container-fluid").append(`<div class="time-block row"><div class="hour col-1">${i < 12 ? `${i}AM` : i > 12 ? `${i-12}PM` : "12PM"}</div><textarea name="" id="" class="description col-10"></textarea><button class="saveBtn col-1">Save</button></div>`)
+// get this from local storage
+var todos = {
+    9: "",
+    10: "",
+    11: "",
+    12: "",
+    13: "",
+    14: "",
+    15: "",
+    16: "",
+    17: "",
+    18: "",
 }
+
+function render() {
+
+    for (var i = 9; i < 18; i++) {
+        $(".container-fluid").append(`
+        <div class="time-block row">
+        <div class="hour col-1 text-start pt-3">${i < 12 ? `${i}AM` : i > 12 ? `${i-12}PM` : "12PM"}</div>
+        <textarea 
+        name="" 
+        id="" 
+        class="description col-10 ${i < currentHour ? 'past' : i === currentHour ? 'present' : 'future'}">${todos[i] || ""}
+        </textarea>
+        <button class="saveBtn col-1">Save</button>
+        </div>
+        `)
+    }
+    
+    
+}
+
+render()
+
+//when saving todo update local storage
